b3a9405d13bd50654f71ea374a09b7e86148a0c3
Aura4G <aria4games@googlemail.com>
2026-07-17 15:50:40 +0100

    Set up blog post writing and pages

---
+++ src/content.config.ts
@@ -0,0 +1,26 @@
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+    loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.date(),
+        tags: z.array(z.string()).optional(),
+    }),
+});
+
+const projects = defineCollection({
+    loader: glob({ pattern: '**/*.md', base: './src/content/projects' }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        tags: z.array(z.string()).optional(),
+        link: z.url().optional(),
+        repo: z.url().optional(),
+    }),
+});
+
+export const collections = { blog, projects };
---
+++ src/content/blog/archandroid.md
@@ -0,0 +1,8 @@
+---
+title: "The Archandroid - Janelle Monae"
+description: "New fav album??"
+pubDate: 2026-07-17
+tags: ["Music", "Review"]
+---
+
+Listening to the Archandroid by Janelle Monae for the first time right now. Will post more thoughts on it later, but GOD DAMN does this sound incredible so far!!!
